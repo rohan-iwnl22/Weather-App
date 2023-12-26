89aa1092310bbac353beabac40ba10617c823780
rohan-iwnl22 <147467039+rohan-iwnl22@users.noreply.github.com>
2023-12-26 14:08:06 +0530

    Add files via upload

---
+++ weatherApp/weather/views.py
@@ -0,0 +1,36 @@
+from django.shortcuts import render
+import requests
+from .models import City
+from .forms import CityForm
+# Create your views here.
+def index(request):
+        url = 'http://api.openweathermap.org/data/2.5/weather?q={}&units=imperial&appid=109d80a73c600fc776ead5f8d3202ebc'
+
+        city = 'rock island'
+        
+
+        if request.method == 'POST':
+                form = CityForm(request.POST)
+                form.save()
+                
+
+        form = CityForm()
+
+        cities = City.objects.all()
+
+        weather_data = []
+        for city in cities:
+                r = requests.get(url.format(city)).json()
+
+                city_weather={
+                        'city': city.name,
+                        'temperature': r["main"]["temp"],
+                        'description': r["weather"][0]["description"],
+                        'icon': r["weather"][0]["icon"]
+                }
+
+                weather_data.append(city_weather)
+
+        context = {'weather-data': weather_data, 'form':form}
+
+        return render(request, 'weather/weather.html', context)
